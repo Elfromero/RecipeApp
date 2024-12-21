7d2b7987dab345eab0b1c02f9eba0beaccc5be59
elfromero <elfromero@gmail.com>
2024-12-21 15:27:38 -0600

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Roman Radchuk on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
