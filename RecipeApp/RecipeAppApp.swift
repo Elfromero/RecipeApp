eabccb2666e6f6c0eb0a623d6737d48896472533
elfromero <elfromero@gmail.com>
2024-12-22 17:02:24 -0600

    Replaced initial presentation.

--- RecipeApp/RecipeAppApp.swift
+++ RecipeApp/RecipeAppApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct RecipeAppApp: App {
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RecipesListView(recipes: [])
         }
     }
 }
